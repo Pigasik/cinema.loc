911ac4a59245516b490983359445cfc78d90c471
VeronikaPigasova <vpigasova96@gmail.com>
2023-02-13 20:13:45 +0300

    int 1

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Movie;
+use App\Models\User;
+use App\Models\Serie;
+use App\Models\Cast;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+   public function index(){
+    $users = User::all();
+    $movies = Movie::all();
+    $series = Serie::all();
+    $casts = Cast::all();
+    return view('admin.index', compact('users', 'movies', 'series', 'casts'));
+   }
+}
