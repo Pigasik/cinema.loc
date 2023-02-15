decee6d1e12bd723692cd5fdee620a0cb0a8e713
VeronikaPigasova <vpigasova96@gmail.com>
2023-02-15 18:26:56 +0300

    casts

--- app/Http/Livewire/CastIndex.php
+++ app/Http/Livewire/CastIndex.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Cast;
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
+use Livewire\WithPagination;
+use Illuminate\Support\Str;
 
 class CastIndex extends Component
 {
+    use WithPagination;
+    protected $key = '1d7a93431099a07e3032306e6032da45';
+    public $castTMDBId;
+    public $castName;
+    public $castPoster;
+
+    public function generateCast(){
+        $newCast = Http::get('https://api.themoviedb.org/3/person/10580?api_key=1d7a93431099a07e3032306e6032da45&language=en-USS')->json();
+        Cast::create([
+            'tmdb_id' => $newCast['id'],
+            'name'    => $newCast['name'],
+            'slug'    => Str::slug($newCast['name']),
+            'poster_path' => $newCast['profile_path']
+        ]);
+    }
+    
     public function render()
     {
-        return view('livewire.cast-index');
+        return view('livewire.cast-index', [
+            'casts' => Cast::paginate(10)
+        ]);
     }
 }
--- resources/views/livewire/cast-index.blade.php
+++ resources/views/livewire/cast-index.blade.php
@@ -1,7 +1,13 @@
 <div class="flex flex-col">
-    <div class="w-full flex mb-4 p-2 justify-end">
-        <x-jet-button>Create Cast</x-jet-button>
+  <div class="w-full flex mb-4 p-2 justify-end">
+    <div class="flex justify-center">
+      <div class="mb-1 w-full">
+        <label for="tmdb_id_g" class="form-label inline-block mb-2 text-gray-700">Cast Name</label>
+        <input wire:model="castTMDBId" id="tmdb_id_g" name="tmdb_id_g" type="text" class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"/>
+      </div>
     </div>
+    <x-jet-button wire:click="generateCast">Create Cast</x-jet-button>  
+  </div>
     <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div class="py-4 inline-block min-w-full sm:px-6 lg:px-8">
         <div class="overflow-hidden">
@@ -9,32 +15,31 @@
             <thead class="border-b bg-gray-800">
               <tr>
                 <th scope="col" class="text-sm font-medium text-white px-6 py-4">
-                  Title
+                  Name
                 </th>
                 <th scope="col" class="text-sm font-medium text-white px-6 py-4">
-                  Date
+                  Slug
                 </th>
                 <th scope="col" class="text-sm font-medium text-white px-6 py-4">
-                  Rating
+                  Poster
                 </th>
                 <th scope="col" class="text-sm font-medium text-white px-6 py-4">
-                  Public
-                </th>
-                <th scope="col" class="text-sm font-medium text-white px-6 py-4">
-                    Action
+                  Action
                 </th>
               </tr>
             </thead class="border-b">
             <tbody>
+              @foreach ($casts as $cast)
               <tr class="bg-white border-b">
                 <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">Title</td>
                 <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">Date</td>
                 <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">Rating</td>
                 <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">Public</td>
                 <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">Edit and Delete</td>
-                
-               
+              </tr>
+               @endforeach
             </tbody>
+            {{$casts->links()}}
           </table>
         </div>
       </div>
