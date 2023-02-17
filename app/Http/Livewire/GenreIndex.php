0a1d5c73f52892e127b78946d4848c8b5cf331f5
VeronikaPigasova <vpigasova96@gmail.com>
2023-02-17 20:35:41 +0300

    fix genre api

--- app/Http/Livewire/GenreIndex.php
+++ app/Http/Livewire/GenreIndex.php
@@ -21,7 +21,7 @@ class GenreIndex extends Component
     public $showEdit = false;
 
     public function generateGenre(){
-        $newGenre = Http::get('https://api.themoviedb.org/3/genre/'. $this->tmdbId .'?api_key=1d7a93431099a07e3032306e6032da45&language=en-US')->json();
+        $newGenre = Http::get('https://api.themoviedb.org/3/genre/movie/list?api_key=1d7a93431099a07e3032306e6032da45&language=en-US')->json();
         $genre = Genre::where('tmdb_id', $newGenre['id'])->first();
         if(!$genre){
             Genre::create([
@@ -31,10 +31,8 @@ class GenreIndex extends Component
         ]);
         $this->reset();
         } else{
-            $this->dispatchBrowserEvent('banner-message', ['style' => 'denger', 'message' => 'Invalid TMDBId']);
-    
+            $this->dispatchBrowserEvent('banner-message', ['style' => 'danger', 'message' => 'Invalid TMDBId']);
         }
-        
     }
 
     public function editGenre($id){
